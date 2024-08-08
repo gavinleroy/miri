3bcab3a4d8b14ea511a1c537f5753c75ad8ec80d
gavinleroy <gavinleroy6@gmail.com>
2024-08-08 12:30:54 -0400

    cfg feature wasi_ext

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@
 #![feature(trait_upcasting)]
 #![feature(strict_overflow_ops)]
 #![feature(strict_provenance)]
+#![cfg(target_os = "wasi")]
 #![feature(wasi_ext)]
 // Configure clippy and other lints
 #![allow(
